949308793cd9a42b291d718c45633fa2b5df7031
Helensiu <148872035+Helensiu@users.noreply.github.com>
2024-01-23 10:24:27 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Write your JavaScript script here.
+// var navbar=
+// document.getElementById('myNavbar');
+
+// window.addEventListener('scroll', function(){
+//     if (window.scrollY>0) {
+//         navbar.classList.add('hover-effect');
+//     } else {
+//         navbar.classList.remove('hover-effect');
+//     }
+// }); 
+
+
+
